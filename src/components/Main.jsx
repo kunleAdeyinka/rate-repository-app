b214223bf71b0f4638cc3e21c676a938f85a817b
ADEKUNLE ADEYINKA <aadeyink@gmail.com>
2022-06-06 18:09:25 -0400

    ran first linting

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,23 +1,22 @@
-import Constants from 'expo-constants'
-import { Text, StyleSheet, View } from 'react-native'
+import Constants from "expo-constants";
+import { StyleSheet, View } from "react-native";
 
-import RepositoryList from './RepositoryList';
+import RepositoryList from "./RepositoryList";
 
 const styles = StyleSheet.create({
-    container: {
-        marginTop: Constants.statusBarHeight,
-        flexGrow: 1,
-        flexShrink: 1,
-    },
+  container: {
+    marginTop: Constants.statusBarHeight,
+    flexGrow: 1,
+    flexShrink: 1,
+  },
 });
 
-
 const Main = () => {
-    return (
-        <View style={styles.container}>
-            <RepositoryList />
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <RepositoryList />
+    </View>
+  );
 };
 
 export default Main;
